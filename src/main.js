9a905464be9398157820c48de19240749d7e8dc7
weixiang <463460902@qq.com>
2021-05-17 17:21:50 +0800

    引入element-plus

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import locale from 'element-plus/lib/locale/lang/zh-cn'
+import 'dayjs/locale/zh-cn'
+createApp(App).use(store).use(router).use(ElementPlus,{locale}).mount('#app')
